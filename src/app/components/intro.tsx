bd6b71d395974f814873443ec55656e6e57e0079
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2024-02-14 17:20:34 +0100

    change main text bg

--- src/app/components/intro.tsx
+++ src/app/components/intro.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 
 const Intro = () => {
 	return (
-		<section className='relative flex bg-black justify-center items-center w-[100%] h-[88vh] sm:h-[65vh] bg-opacity-20'>
+		<section className='relative flex bg-black justify-center items-center w-[100%] h-[65vh] sm:h-[65vh] bg-opacity-20'>
 			<Image
 				src='/test.jpg'
 				alt='Zdjęcie świątyni w Licheniu'
@@ -13,9 +13,9 @@ const Intro = () => {
 				priority
 				className='object-cover z-[-3] '
 			/>
-			<div className='relative flex flex-col justify-center md:items-center h-[88vh] sm:h-[65vh] w-[100%]'>
+			<div className='relative flex flex-col justify-center md:items-center h-[65vh] sm:h-[65vh] w-[100%]'>
 				<motion.div
-					className='absolute top-0 uppercase text-end md:text-xl p-4 text-second-color bg-main-color bg-opacity-50 tracking-wider w-[100%]'
+					className='absolute top-0 uppercase text-end md:text-xl p-2 text-second-color bg-black bg-opacity-50 tracking-wider w-[100%]'
 					initial={{ x: '-50px', opacity: 0 }}
 					animate={{ x: 0, opacity: 1 }}
 					transition={{ ease: 'easeOut', duration: 0.5, delay: 0.5 }}
@@ -32,7 +32,7 @@ const Intro = () => {
 					initial={{ x: '-50px', opacity: 0 }}
 					animate={{ x: 0, opacity: 1 }}
 					transition={{ ease: 'easeOut', duration: 0.5, delay: 1 }}
-					className='absolute bottom-0 flex justify-center text-sm md:text-lg lg:text-xl xl:text-4xl font-semibold text-center p-4 text-second-color bg-main-color bg-opacity-50 tracking-wider w-[100%] uppercase '
+					className='absolute bottom-0 flex justify-center text-sm md:text-lg lg:text-xl xl:text-4xl font-semibold text-center p-4 text-second-color bg-black bg-opacity-50 tracking-wider w-[100%] uppercase '
 				>
 					&quot;Pielgrzymuj z pasją, odkrywaj z wiarą!&quot;
 				</motion.p>
